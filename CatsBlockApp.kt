744798f8b90d0f579e6466b6fc2bd7c8260e3092
catsblock <castilhoisaac2@gmail.com>
2026-07-21 23:39:29 -0300

    Add files via upload

---
+++ CatsBlockApp.kt
@@ -0,0 +1,14 @@
+package com.catsblock.phone
+
+import android.app.Application
+
+/**
+ * Classe principal de Application do CatsBlock Phone.
+ * Inicializa componentes globais e banco de dados local.
+ */
+class CatsBlockApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Inicialização de serviços e configurações do aplicativo
+    }
+}
