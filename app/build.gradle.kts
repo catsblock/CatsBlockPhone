119c043e5cad9402c2daceea3c187ea8eb752e07
catsblock <castilhoisaac2@gmail.com>
2026-07-22 00:24:31 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.google.devtools.ksp) // Alterado para ler do seu libs.versions.toml
 }
 
 android {
